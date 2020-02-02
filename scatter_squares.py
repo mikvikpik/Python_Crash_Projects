e95bbde6a2da567898b7cecf3dbbf84586270590
Andrew Mikol <andrewmikol@yahoo.com>
2020-02-01 22:34:44 -0600

    February 1, 2020 Python Crash Project

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 x_values = list(range(1,1000))
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, s = 40, edgecolor = 'none')
+plt.scatter(x_values, y_values, s = 40, edgecolor = 'none', c = y_values,
+            cmap = plt.cm.Blues)
 
 # Set chart title and lebel axes.
 plt.title("Square Numbers", fontsize = 24)
@@ -17,3 +18,5 @@ plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
 plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
+
+plt.savefig('squares_plot.png', bbox_inches = 'tight')
